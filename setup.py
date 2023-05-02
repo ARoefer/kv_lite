9292635c74af54a8b87d25315c57ee97a2ae062a
Adrian Röfer <aroefer@cs.uni-freiburg.de>
2023-05-02 09:17:51 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Try catkin install
+try:
+   from catkin_pkg.python_setup import generate_distutils_setup
+   from distutils.core import setup
+
+   d = generate_distutils_setup(
+      packages=['kineverse'],
+      package_dir={'': 'src'}
+   )
+
+   setup(**d)
+
+# Switch to regular pip install
+except ModuleNotFoundError:
+   from pathlib    import Path
+   from setuptools import setup, find_packages
+
+   with open(f'{Path(__file__).parent}/requirements.txt', 'r') as f:
+      pip_dependencies = f.readlines()
+
+   setup(
+      name='Kineverse V2',
+      version='0.1.0',
+      author='Adrian Roefer',
+      author_email='aroefer@cs.uni-freiburg.de',
+      packages=['kineverse'],
+      package_dir={'': 'src'},
+      # scripts=['bin/script1','bin/script2'],
+      url='http://pypi.python.org/pypi/kineverse/',
+      license='LICENSE',
+      description='A second attempt at implementing the kineverse concept.',
+      # long_description=open('README.txt').read(),
+      install_requires=pip_dependencies
+   )
