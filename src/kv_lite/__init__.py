a44164530595457c0cdb21cc13c3b0c02ba5e8dd
Adrian Roefer <aroefer@uni-freiburg.de>
2023-06-21 15:36:49 +0200

    Exposed exp_utils in package root.

--- src/kv_lite/__init__.py
+++ src/kv_lite/__init__.py
@@ -1,5 +1,6 @@
 from . import spatial    as gm
 from . import urdf_utils as urdf
+from . import exp_utils  as exp
 
 from .graph import FKChainException, \
                    Frame,            \
--- src/kv_lite/exp_utils.py
+++ src/kv_lite/exp_utils.py
@@ -16,12 +16,11 @@ def twist_to_se3(q, v, w, epsilon=1e-6) -> gm.KVArray:
                         [ w[2, 0],        0, -w[0, 0]],
                         [-w[1, 0],  w[0, 0],       0]])
     e_wq  = gm.eye(3) + gm.sin(q) * w_hat + w_hat.dot(w_hat) * (1 - gm.cos(q))
-    h     = w.T.dot(v[:3]) / (gm.norm(w**2) + epsilon)
-    lin   = (gm.eye(3) - e_wq).dot(gm.cross(w[:3], v[:3])[:3]) + w.dot(w.T)[:3].dot(v[:3]) * q
-    
+    h     = w[:3].T.dot(v[:3]) / (gm.norm(w[:3]**2) + epsilon)
+    lin   = (gm.eye(3) - e_wq).dot(gm.cross(w[:3], v[:3])[:3]) + w[:3].dot(w[:3].T)[:3].dot(v[:3]) * q
     tf         = gm.eye(4).astype(object)
     tf[:3, :3] = e_wq
-    tf[:3,  3] = lin
+    tf[:3,  3] = lin.flatten()
 
     return tf
 
