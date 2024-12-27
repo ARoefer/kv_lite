533581c4d61ced218588c0718bb366e93069b854
Adrian Röfer <aroefer@uni-bremen.de>
2024-12-27 15:46:22 +0100

    Added registration of KV types to module initializations

--- src/kv_lite/__init__.py
+++ src/kv_lite/__init__.py
@@ -23,3 +23,29 @@ from .model import Model,           \
                    Body, \
                    Geometry, \
                    Inertial
+
+
+def __init_ros_serialization():
+    try:
+        from roebots import ROS_SERIALIZER
+        from roebots.ros_serializer import serialize_np_matrix_quat, \
+                                           serialize_np_4x4_pose, \
+                                           serialize_np_4x1_matrix 
+
+        from geometry_msgs.msg import Quaternion as QuaternionMsg, \
+                                      Pose       as PoseMsg,       \
+                                      Point      as PointMsg,      \
+                                      Vector3    as Vector3Msg
+
+        def floatify(f):
+            def g(m):
+                return f(m.astype(float))
+            return g
+
+        ROS_SERIALIZER.add_serializer(floatify(serialize_np_matrix_quat), {gm.KVArray}, {QuaternionMsg})
+        ROS_SERIALIZER.add_serializer(floatify(serialize_np_4x4_pose), {gm.KVArray}, {PoseMsg})
+        ROS_SERIALIZER.add_serializer(floatify(serialize_np_4x1_matrix), {gm.KVArray}, {PointMsg, Vector3Msg})
+    except ModuleNotFoundError:
+        pass
+
+__init_ros_serialization()
