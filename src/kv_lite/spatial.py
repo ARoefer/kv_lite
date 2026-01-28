719b8ddf57ae127a11a2c9a24db4f6a314093ead
Adrian Röfer <aroefer@cs.uni-freiburg.de>
2026-01-28 18:25:45 +0100

    Bugfix: Incomplete type check

--- src/kv_lite/spatial.py
+++ src/kv_lite/spatial.py
@@ -182,7 +182,7 @@ class Transform:
     @staticmethod
     def inverse(tf):
         inv = eye(4)
-        inv = inv.astype(object) if tf.is_symbolic else inv
+        inv = inv.astype(object) if isinstance(tf, KVArray) and tf.is_symbolic else inv
         inv[..., :3, :3] = tf[..., :3, :3].T
         inv[..., :3,  3] = -inv[..., :3, :3].dot(tf[..., :3, 3])
         return inv
