50befe87319a688db66a5fd7f654cb276a05d54d
Adrian Röfer <aroefer@cs.uni-freiburg.de>
2026-01-28 18:25:22 +0100

    Updated QP-controller

--- src/kv_lite/control.py
+++ src/kv_lite/control.py
@@ -10,6 +10,7 @@ except ModuleNotFoundError as NO_QPSOLVERS:
 
 
 from . import spatial as kv
+from . import urdf
 
 class ObjectiveTypes(Enum):
     EQUALITY=0
@@ -90,15 +91,17 @@ def _build_objective_matrix(objectives : dict[str, Objective],
 
 
 class QPController:
-    def __init__(self, robot : kv.urdf.URDFObject,
+    def __init__(self, robot : urdf.URDFObject,
                        objectives : dict[str, Objective],
                        control_symbols : list[kv.KVSymbol],
                        control_costs : dict[kv.KVSymbol],
                        default_control_cost=0.1,
                        dampening_factor : float=None,
+                       vel_bound_scale : float=1.0,
                        position_integration_factor : float=None,
                        regularization_weight : dict[kv.Symbol, float] | float=None,
                        regularization_target : dict[kv.Symbol, float]=None,
+                       lambda_damping : float=0,
                        solver : str='daqp'):
         if solve_qp is None:
             raise ImportError(f'You do not have the "qpsolvers" package installed. Original exception: {NO_QPSOLVERS}')
@@ -112,17 +115,18 @@ class QPController:
         self._position_integration_factor = position_integration_factor
 
         self._dampening_factor = dampening_factor
+        self._lamda_damping = lambda_damping
 
         # 
         if len(self._eq_objectives) > 0:
-            self._A, self._b, _, A_softdims, A_softweights = _build_objective_matrix(self._eq_objectives, control_symbols)
+            self._A, self._b, A_obj_ranges, A_softdims, A_softweights = _build_objective_matrix(self._eq_objectives, control_symbols)
         else:
-            self._A, self._b, A_softdims, A_softweights = None, None, 0, None
+            self._A, self._b, A_obj_ranges, A_softdims, A_softweights = None, None, {}, 0, None
 
         if len(self._ineq_objectives) > 0:
-            self._G, self._h, _, G_softdims, G_softweights = _build_objective_matrix(self._ineq_objectives, control_symbols, slack_padding=A_softdims)
+            self._G, self._h, G_obj_ranges, G_softdims, G_softweights = _build_objective_matrix(self._ineq_objectives, control_symbols, slack_padding=A_softdims)
         else:
-            self._G, self._h, G_softdims, G_softweights = None, None, 0, None
+            self._G, self._h, G_obj_ranges, G_softdims, G_softweights = None, None, {}, 0, None
 
         if self._A is not None and G_softdims > 0:
             self._A = kv.hstack((self._A, kv.zeros((len(self._A), G_softdims))))
@@ -131,7 +135,28 @@ class QPController:
                                          (A_softweights if A_softweights is not None else []),
                                          (G_softweights if G_softweights is not None else []),
                                         )))
-        self._q_vel_limits = np.vstack([dict(zip(robot.q, robot.q_dot_limit)).get(s, [-1e6, 1e6]) for s in self._control_symbols])
+        self._costs += np.eye(len(self._costs)) * 2 * self._lamda_damping
+        self._objective_cost_masks = {}
+        cost_offset = len(control_symbols)
+        for name in A_obj_ranges.keys():
+            if not (o:=self._eq_objectives[name]).is_soft:
+                continue
+
+            cost_diag_mask = np.zeros(len(self._costs), dtype=bool)
+            cost_diag_mask[cost_offset:cost_offset+o.ndim] = True
+            self._objective_cost_masks[name] = cost_diag_mask
+            cost_offset += o.ndim
+
+        for name in G_obj_ranges.keys():
+            if not (o:=self._ineq_objectives[name]).is_soft:
+                continue
+
+            cost_diag_mask = np.zeros(len(self._costs), dtype=bool)
+            cost_diag_mask[cost_offset:cost_offset+o.ndim] = True
+            self._objective_cost_masks[name] = cost_diag_mask
+            cost_offset += o.ndim
+
+        self._q_vel_limits = np.vstack([dict(zip(robot.q, robot.q_dot_limit)).get(s, [-1e6, 1e6]) for s in self._control_symbols]) * vel_bound_scale
         self._q_pos_limits = np.vstack([dict(zip(robot.q, robot.q_limit)).get(s, [-1e6, 1e6])     for s in self._control_symbols])
         self._x_limits     = np.vstack([self._q_vel_limits, [[-1e6, 1e6]]*(G_softdims + A_softdims)])
 
@@ -158,8 +183,22 @@ class QPController:
 
         self.reset()
 
+    @property
+    def robot(self) -> urdf.URDFObject:
+        return self._robot
+
     def reset(self):
         self._last_x_dot = None
+        self._last_P     = None
+        self._last_b     = None
+        self._last_h     = None
+        self._last_q     = None
+
+    def last_objective_costs(self) -> dict[str, np.ndarray]:
+        if self._last_P is None:
+            raise ValueError('Need to eval at least once.')
+        
+        return {n: self._last_P[m, m] for n, m in self._objective_cost_masks.items()}
 
     def eval(self, q_obs : dict[kv.Symbol, float]) -> dict[kv.Symbol, float]:
         # (J(EE_vel))
@@ -174,16 +213,25 @@ class QPController:
             q[:len(q_pos)] = (q_pos - self._regularization_target) * self._regularization_weight
             q[~self._has_reg_target] = 0
 
-        P = self._costs(q_obs)
+        q[:len(self._control_symbols)] -= 2 * self._lamda_damping * self._last_x_dot[:len(self._control_symbols)]
 
+        self._last_q = q
+
+        P = self._costs(q_obs)
+        self._last_P = P
         A = self._A(q_obs) if self._A is not None else None
         b = self._b(q_obs) if self._b is not None else None
-        
+        self._last_b = b
+
         G = self._G(q_obs) if self._G is not None else None
         h = self._h(q_obs) if self._h is not None else None
+        self._last_h = h
 
         if self._dampening_factor is not None:
             limits = (self._x_limits * self._dampening_factor) + self._last_x_dot[..., None]
+            # Always clamp back to the original limits
+            limits = np.asarray([np.max((limits.T[0], self._x_limits.T[0]), axis=0),
+                                 np.min((limits.T[1], self._x_limits.T[1]), axis=0)]).T
         else:
             limits = self._x_limits
 
